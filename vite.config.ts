d124d8160c2b7665e925047862f1c44e587d1b55
Matheus Frej <matheusfrej@hotmail.com>
2024-07-21 16:23:08 -0300

    test: cria test environment e organiza npm scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
+  test: {
+    environmentMatchGlobs: [["src/delivery/api/rest/controller/**", "prisma"]],
+    dir: "src",
+  },
 });
